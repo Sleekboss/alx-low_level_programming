829164a9d7f501ee57906176ffb2e6af6cbd92a9
Albert agyemang <alagyespy@gmail.com>
2022-12-10 02:14:02 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * main Entry point
+ * main - Entry point
  *
- * REturn Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
